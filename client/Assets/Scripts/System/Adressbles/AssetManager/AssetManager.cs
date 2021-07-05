7c83418217acd45af132acbc3cf9a42429cb6ba1
zhangwenbin <991355745@qq.com>
2021-07-05 14:29:08 +0800

    加命名空间EG(EasyGame)

--- client/Assets/Scripts/System/Adressbles/AssetManager/AssetManager.cs
+++ client/Assets/Scripts/System/Adressbles/AssetManager/AssetManager.cs
@@ -6,298 +6,334 @@ using UnityEngine.AddressableAssets;
 using UnityEngine.AddressableAssets.ResourceLocators;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.SceneManagement;
-public class AssetManager : MonoBehaviour
+
+namespace EG
 {
-
-    public enum ReleaseType
+    public class AssetManager : MonoBehaviour
     {
-        OnNewScene,
-        Manual,
-        NoneRelease
-    }
-    public static AssetManager Instance;
-    public bool IsInitialize;
-    private bool enableHotUpdate = false;
-    public Action<int, float> progressHandler;
 
-    public static AsyncOperationHandle defaultHandle;
-
-    private Dictionary<AsyncOperationHandle, GameObject> handleDic = new Dictionary<AsyncOperationHandle, GameObject>();
-
-    private void Awake()
-    {
-        Instance = this;
-        GameObject.DontDestroyOnLoad(gameObject);
-    }
-    public void Initialize()
-    {
-        Addressables.InitializeAsync().Completed += InitialCompleted;
-    }
-
-    private void InitialCompleted(AsyncOperationHandle<IResourceLocator> obj)
-    {
-        if (!enableHotUpdate)
+        public enum ReleaseType
         {
-            IsInitialize = true;
-            return;
+            OnNewScene,
+            Manual,
+            NoneRelease
         }
-        Addressables.CheckForCatalogUpdates(true).Completed += CheckComplete;
-    }
+        public static AssetManager Instance;
+        public bool IsInitialize;
+        private bool enableHotUpdate = false;
 
-    private void CheckComplete(AsyncOperationHandle<List<string>> obj)
-    {
-        if (obj.Status == AsyncOperationStatus.Failed)
+        /// <summary>
+        /// 进度回调
+        /// </summary>
+        public Action<int, float> progressHandler;
+        /// <summary>
+        /// 默认handle
+        /// </summary>
+        public static AsyncOperationHandle defaultHandle;
+
+        /// <summary>
+        /// handle map
+        /// </summary>
+        private Dictionary<AsyncOperationHandle, GameObject> handleDic = new Dictionary<AsyncOperationHandle, GameObject>();
+
+        private void Awake()
         {
-            Debug.LogError(" 检查 catalog失败");
-            return;
+            Instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
         }
-        Debug.Log(obj.Result.Count);
-        if (obj.Result.Count == 0)
+        public void Initialize()
         {
-            DownLoadSize();
-            return;
+            Addressables.InitializeAsync().Completed += InitialCompleted;
         }
-        Addressables.UpdateCatalogs(obj.Result).Completed += UpdateCatalogsComplete;
-    }
 
-
-
-    private void UpdateCatalogsComplete(AsyncOperationHandle<List<IResourceLocator>> obj)
-    {
-        Debug.Log(obj.Status);
-        if (obj.Status == AsyncOperationStatus.Failed)
+        private void InitialCompleted(AsyncOperationHandle<IResourceLocator> obj)
         {
-            return;
-        }
-        DownLoadSize(obj.Result);
-    }
-
-    long updateSize;
-
-    public long GetDownloadSize()
-    {
-        return updateSize;
-    }
-    public void DownLoadSize(List<IResourceLocator> obj = null)
-    {
-        StartCoroutine(DownLoadSizeIe(obj));
-    }
-
-    public IEnumerator DownLoadSizeIe(List<IResourceLocator> obj = null)
-    {
-        long tempSize = 0;
-        updateSize = 0;
-        var locators = Addressables.ResourceLocators;
-        if (obj != null)
-        {
-            locators = obj;
-        }
-        Dictionary<IResourceLocator, long> dic = new Dictionary<IResourceLocator, long>();
-        float index = 0;
-        int count = obj == null ? 2 : obj.Count;
-        foreach (var item in locators)
-        {
-            var handle = Addressables.GetDownloadSizeAsync(item.Keys);
-            yield return handle;
-            if (handle.Result > 0)
+            if (!enableHotUpdate)
             {
-                tempSize += handle.Result;
-                dic.Add(item, handle.Result);
+                IsInitialize = true;
+                return;
             }
-            Addressables.Release(handle);
-            //if (progressHandler != null)
-            //{
-            //    index++;
-            //    progressHandler(0, index/count);
-            //}
-
+            Addressables.CheckForCatalogUpdates(true).Completed += CheckComplete;
         }
-        updateSize = tempSize;
-        float currentDownloadSize = 0;
-        float totalDownLoadSize = 0;
-        float downloadPercent = 0;
-        if (updateSize > 0)
+
+        #region 热更
+        private void CheckComplete(AsyncOperationHandle<List<string>> obj)
         {
-            foreach (var item in dic)
+            if (obj.Status == AsyncOperationStatus.Failed)
             {
-                Debug.Log("download " + item.Key);
-                var downloadHandle = Addressables.DownloadDependenciesAsync(item.Key.Keys, Addressables.MergeMode.Union);
-                while (!downloadHandle.IsDone)
+                Debug.LogError(" 检查 catalog失败");
+                return;
+            }
+            Debug.Log(obj.Result.Count);
+            if (obj.Result.Count == 0)
+            {
+                DownLoadSize();
+                return;
+            }
+            Addressables.UpdateCatalogs(obj.Result).Completed += UpdateCatalogsComplete;
+        }
+
+
+
+        private void UpdateCatalogsComplete(AsyncOperationHandle<List<IResourceLocator>> obj)
+        {
+            Debug.Log(obj.Status);
+            if (obj.Status == AsyncOperationStatus.Failed)
+            {
+                return;
+            }
+            DownLoadSize(obj.Result);
+        }
+
+        long updateSize;
+
+        public long GetDownloadSize()
+        {
+            return updateSize;
+        }
+        public void DownLoadSize(List<IResourceLocator> obj = null)
+        {
+            StartCoroutine(DownLoadSizeIe(obj));
+        }
+
+        public IEnumerator DownLoadSizeIe(List<IResourceLocator> obj = null)
+        {
+            long tempSize = 0;
+            updateSize = 0;
+            var locators = Addressables.ResourceLocators;
+            if (obj != null)
+            {
+                locators = obj;
+            }
+            Dictionary<IResourceLocator, long> dic = new Dictionary<IResourceLocator, long>();
+            float index = 0;
+            int count = obj == null ? 2 : obj.Count;
+            foreach (var item in locators)
+            {
+                var handle = Addressables.GetDownloadSizeAsync(item.Keys);
+                yield return handle;
+                if (handle.Result > 0)
                 {
-                    currentDownloadSize = downloadHandle.PercentComplete * item.Value;
-                    downloadPercent = (totalDownLoadSize + currentDownloadSize) / updateSize;
-                    if (progressHandler != null)
-                    {
-                        progressHandler(0, downloadPercent);
-                    }
-                    yield return null;
+                    tempSize += handle.Result;
+                    dic.Add(item, handle.Result);
                 }
-                totalDownLoadSize += currentDownloadSize;
-                Addressables.Release(downloadHandle);
+                Addressables.Release(handle);
+                //if (progressHandler != null)
+                //{
+                //    index++;
+                //    progressHandler(0, index/count);
+                //}
+
             }
-            if (progressHandler != null)
+            updateSize = tempSize;
+            float currentDownloadSize = 0;
+            float totalDownLoadSize = 0;
+            float downloadPercent = 0;
+            if (updateSize > 0)
             {
-                progressHandler(0, 1);
-            }
-            IsInitialize = true;
-        }
-        else
-        {
-            IsInitialize = true;
-        }
-
-
-    }
-
-    /// <summary>
-    /// 不需要加载的资源
-    /// </summary>
-    Dictionary<string, GameObject> staticPool = new Dictionary<string, GameObject>();
-
-    /// <summary>
-    /// 注册不需要加载的资源
-    /// </summary>
-    /// <param name="key"></param>
-    /// <param name="obj"></param>
-    public virtual void RegisterAsset(string key, GameObject obj)
-    {
-        if (obj!=null)
-        {
-            staticPool[key] = obj;
-        }
-       
-    }
-
-    /// <summary>
-    /// 有可能卡死
-    /// </summary>
-    /// <param name="key"></param>
-    /// <param name="callback"></param>
-    public T LoadAssetSync<T>(string key) where T : UnityEngine.Object
-    {
-        //if (staticPool.ContainsKey(key))
-        //{
-        //    return staticPool[key] as T;
-        //}
-        //var item = GameObjectPool.GetGameObject(key);
-        //if (item != null)
-        //{
-        //    item.SetActive(true);
-        //    return item as T;
-        //}
-        //var refObj = RefPool<T>.GetObject(key);
-        //if (refObj != null)
-        //{
-        //    return refObj;
-        //}
-        var op = Addressables.LoadAssetAsync<T>(key);
-        var res = op.WaitForCompletion();
-
-        return res;
-    }
-
-
-    public AsyncOperationHandle LoadAssetAsync<T>(string key, Action<string, T> callback) where T : UnityEngine.Object
-    {
-        if (staticPool.ContainsKey(key))
-        {
-            if (callback != null)
-                callback(key, staticPool[key] as T);
-            return defaultHandle;
-        }
-        var item = GameObjectPool.GetGameObject(key);
-        if (item != null)
-        {
-            item.SetActive(true);
-            if (callback != null)
-                callback(key, item as T);
-            return defaultHandle;
-        }
-
-        var op = Addressables.LoadAssetAsync<T>(key);
-        op.Completed += (res) =>
-        {
-            if (callback != null)
-            {
-                if (res.Status == AsyncOperationStatus.Succeeded)
+                foreach (var item in dic)
                 {
-                    GameObject go = res.Result as GameObject;
-                    if (go)
+                    Debug.Log("download " + item.Key);
+                    var downloadHandle = Addressables.DownloadDependenciesAsync(item.Key.Keys, Addressables.MergeMode.Union);
+                    while (!downloadHandle.IsDone)
                     {
+                        currentDownloadSize = downloadHandle.PercentComplete * item.Value;
+                        downloadPercent = (totalDownLoadSize + currentDownloadSize) / updateSize;
+                        if (progressHandler != null)
+                        {
+                            progressHandler(0, downloadPercent);
+                        }
+                        yield return null;
+                    }
+                    totalDownLoadSize += currentDownloadSize;
+                    Addressables.Release(downloadHandle);
+                }
+                if (progressHandler != null)
+                {
+                    progressHandler(0, 1);
+                }
+                IsInitialize = true;
+            }
+            else
+            {
+                IsInitialize = true;
+            }
+
+
+        }
+
+        #endregion
+        /// <summary>
+        /// 不需要加载的资源
+        /// </summary>
+        Dictionary<string, GameObject> staticPool = new Dictionary<string, GameObject>();
+
+        /// <summary>
+        /// 注册不需要加载的资源
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="obj"></param>
+        public virtual void RegisterAsset(string key, GameObject obj)
+        {
+            if (obj != null)
+            {
+                staticPool[key] = obj;
+            }
+
+        }
+
+        /// <summary>
+        /// 有可能卡死
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="callback"></param>
+        public T LoadAssetSync<T>(string key) where T : UnityEngine.Object
+        {
+            //if (staticPool.ContainsKey(key))
+            //{
+            //    return staticPool[key] as T;
+            //}
+            //var item = GameObjectPool.GetGameObject(key);
+            //if (item != null)
+            //{
+            //    item.SetActive(true);
+            //    return item as T;
+            //}
+            //var refObj = RefPool<T>.GetObject(key);
+            //if (refObj != null)
+            //{
+            //    return refObj;
+            //}
+            var op = Addressables.LoadAssetAsync<T>(key);
+            var res = op.WaitForCompletion();
+
+            return res;
+        }
+
+        /// <summary>
+        /// 异步加载资源
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="callback"></param>
+        /// <returns></returns>
+        public AsyncOperationHandle LoadAssetAsync<T>(string key, Action<string, T> callback) where T : UnityEngine.Object
+        {
+            if (staticPool.ContainsKey(key))
+            {
+                if (callback != null)
+                    callback(key, staticPool[key] as T);
+                return defaultHandle;
+            }
+            var item = GameObjectPool.GetGameObject(key);
+            if (item != null)
+            {
+                item.SetActive(true);
+                if (callback != null)
+                    callback(key, item as T);
+                return defaultHandle;
+            }
+
+            var op = Addressables.LoadAssetAsync<T>(key);
+            op.Completed += (res) =>
+            {
+                if (callback != null)
+                {
+                    if (res.Status == AsyncOperationStatus.Succeeded)
+                    {
+                        GameObject go = res.Result as GameObject;
+                        if (go)
+                        {
                         //if (EngineDelegate.PrefabUnInstantiateRule != null && EngineDelegate.PrefabUnInstantiateRule(go))
                         //{
                         //    callback(key, go as T);
                         //}
                         //else
                         {
-                            handleDic[op] = UnityEngine.GameObject.Instantiate(go);
-                            callback(key, handleDic[op] as T);
-                        }
+                                handleDic[op] = UnityEngine.GameObject.Instantiate(go);
+                                callback(key, handleDic[op] as T);
+                            }
 
+                        }
+                        else
+                        {
+                            callback(key, res.Result);
+                        }
                     }
                     else
                     {
-                        callback(key, res.Result);
+                        Debug.LogError("加载资源失败 " + key);
+                        callback(key, null);
                     }
+
+                }
+            };
+
+            return op;
+        }
+
+        /// <summary>
+        /// 异步加载场景
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="callback"></param>
+        /// <param name="loadSceneMode"></param>
+        /// <param name="activateOnLoad"></param>
+        /// <param name="priority"></param>
+        public void LoadSceneAsync(string key, Action<string> callback, LoadSceneMode loadSceneMode = LoadSceneMode.Single, bool activateOnLoad = true, int priority = 100)
+        {
+            Addressables.LoadSceneAsync(key, loadSceneMode, activateOnLoad, priority).Completed += (res) =>
+            {
+
+                if (callback != null)
+                {
+                    callback(key);
+                }
+
+            };
+        }
+
+        /// <summary>
+        /// 释放资源
+        /// </summary>
+        /// <param name="handle"></param>
+        /// <param name="forece"></param>
+        public void ReleaseHandle(AsyncOperationHandle handle, bool forece = false)
+        {
+            if (forece)
+            {
+                Addressables.Release(handle);
+                return;
+            }
+            GameObject go;
+            if (handleDic.TryGetValue(handle, out go))
+            {
+                if (go != null)
+                {
+                    //加载的资源还在使用,不能释放,只能update释放
+                    return;
                 }
                 else
                 {
-                    Debug.LogError("加载资源失败 " + key);
-                    callback(key, null);
+                    handleDic.Remove(handle);
                 }
-
             }
-        };
-
-        return op;
-    }
-
-
-    public void LoadSceneAsync(string key, Action<string> callback, LoadSceneMode loadSceneMode = LoadSceneMode.Single, bool activateOnLoad = true, int priority = 100)
-    {
-        Addressables.LoadSceneAsync(key, loadSceneMode, activateOnLoad, priority).Completed += (res) =>
-        {
-
-            if (callback != null)
-            {
-                callback(key);
-            }
-
-        };
-    }
-
-    public void ReleaseHandle(AsyncOperationHandle handle,bool forece=false)
-    {
-        if (forece)
-        {
             Addressables.Release(handle);
-            return;
         }
-        GameObject go;
-        if (handleDic.TryGetValue(handle,out go))
-        {
-            if (go!=null)
-            {
-                //加载的资源还在使用,不能释放,只能update释放
-                return;
-            }
-            else
-            {
-                handleDic.Remove(handle);
-            }
-        }
-        Addressables.Release(handle);
-    }
 
-    public void UpdateHandles()
-    {
-        foreach (var item in handleDic)
+        /// <summary>
+        /// 更新handle
+        /// </summary>
+        public void UpdateHandles()
         {
-            if (item.Value==null)
+            foreach (var item in handleDic)
             {
-                handleDic.Remove(item.Key);
-                ReleaseHandle(item.Key);
-                return;
+                if (item.Value == null)
+                {
+                    handleDic.Remove(item.Key);
+                    ReleaseHandle(item.Key);
+                    return;
+                }
             }
         }
     }
--- client/Assets/Scripts/System/Adressbles/AssetManager/GameObjectPool.cs
+++ client/Assets/Scripts/System/Adressbles/AssetManager/GameObjectPool.cs
@@ -4,128 +4,131 @@ using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 
-public static class GameObjectPool 
+namespace EG
 {
-    private static Dictionary<string , CacheItem> pools = new Dictionary<string, CacheItem>();//gameobject 缓存池
-    public static void RecycleGameObject(string assetName,GameObject go)
+    public static class GameObjectPool
     {
-        CacheItem item = null;
-        if (!pools.TryGetValue(assetName,out item))
+        private static Dictionary<string, CacheItem> pools = new Dictionary<string, CacheItem>();//gameobject 缓存池
+        public static void RecycleGameObject(string assetName, GameObject go)
         {
-            item = new CacheItem();
-            pools[assetName] = item;
-        }
-        if (!item.PutIn(go))
-        {
-            GameObject.Destroy(go);
-        }
-    }
-
-    public static GameObject GetGameObject(string assetName)
-    {
-        CacheItem item = null;
-        if (pools.TryGetValue(assetName, out item))
-        {
-            return item.Get();
-        }
-        return null;
-    }
-
-  
-
-    public class CacheItem
-    {
-        public CacheItem()
-        {
-            maxCount = 8;
-            array = new GameObject[maxCount];
-            index = -1;
-        }
-        private int index;
-        public int maxCount;
-        public GameObject[] array;
-        public bool PutIn(GameObject go)
-        {
-            if (index<maxCount-1)
+            CacheItem item = null;
+            if (!pools.TryGetValue(assetName, out item))
             {
-                index++;
-                array[index] = go;
-                go.SetActive(false);
-                return true;
+                item = new CacheItem();
+                pools[assetName] = item;
             }
-            return false;
+            if (!item.PutIn(go))
+            {
+                GameObject.Destroy(go);
+            }
         }
 
-        public GameObject Get()
+        public static GameObject GetGameObject(string assetName)
         {
-            if (index >=0)
+            CacheItem item = null;
+            if (pools.TryGetValue(assetName, out item))
             {
-                index--;
-                return array[index + 1];
+                return item.Get();
             }
             return null;
         }
+
+
+
+        public class CacheItem
+        {
+            public CacheItem()
+            {
+                maxCount = 8;
+                array = new GameObject[maxCount];
+                index = -1;
+            }
+            private int index;
+            public int maxCount;
+            public GameObject[] array;
+            public bool PutIn(GameObject go)
+            {
+                if (index < maxCount - 1)
+                {
+                    index++;
+                    array[index] = go;
+                    go.SetActive(false);
+                    return true;
+                }
+                return false;
+            }
+
+            public GameObject Get()
+            {
+                if (index >= 0)
+                {
+                    index--;
+                    return array[index + 1];
+                }
+                return null;
+            }
+        }
+
     }
 
-}
-
-public static class RefPool<T> where T:UnityEngine.Object
-{
-    private static Dictionary<string, RefItem<T>> refPools = new Dictionary<string, RefItem<T>>();//引用类型缓存池
-    public static void OnLoadObject(string assetName, T obj, AsyncOperationHandle handle) 
+    public static class RefPool<T> where T : UnityEngine.Object
     {
-        RefItem<T> item = null;
-        if (!refPools.TryGetValue(assetName, out item))
+        private static Dictionary<string, RefItem<T>> refPools = new Dictionary<string, RefItem<T>>();//引用类型缓存池
+        public static void OnLoadObject(string assetName, T obj, AsyncOperationHandle handle)
         {
-            item = new RefItem<T>(obj, handle);
-            item.AddRef();
-            refPools[assetName] = item;
+            RefItem<T> item = null;
+            if (!refPools.TryGetValue(assetName, out item))
+            {
+                item = new RefItem<T>(obj, handle);
+                item.AddRef();
+                refPools[assetName] = item;
+            }
         }
+
+        public static T GetObject(string assetName)
+        {
+            RefItem<T> item = null;
+            if (refPools.TryGetValue(assetName, out item))
+            {
+                item.AddRef();
+                return item.obj;
+            }
+            return null;
+        }
+        public class RefItem<U> where U : UnityEngine.Object
+        {
+            public U obj;
+            public AsyncOperationHandle handle;
+            public int refNum = 0;
+            public RefItem(U obj, AsyncOperationHandle handle)
+            {
+                this.obj = obj;
+                this.handle = handle;
+                refNum = 0;
+            }
+
+            public void AddRef()
+            {
+                refNum++;
+            }
+
+            public void SubRef()
+            {
+                refNum--;
+            }
+
+            public bool CanRelease()
+            {
+                return refNum <= 0;
+            }
+
+            public void Release()
+            {
+                refNum = 0;
+                obj = null;
+                Addressables.Release(handle);
+            }
+        }
+
     }
-
-    public static T GetObject(string assetName)
-    {
-        RefItem<T> item = null;
-        if (refPools.TryGetValue(assetName, out item))
-        {
-            item.AddRef();
-            return item.obj;
-        }
-        return null;
-    }
-    public class RefItem<U> where U : UnityEngine.Object
-    {
-        public U obj;
-        public AsyncOperationHandle handle;
-        public int refNum = 0;
-        public RefItem(U obj, AsyncOperationHandle handle)
-        {
-            this.obj = obj;
-            this.handle = handle;
-            refNum = 0;
-        }
-
-        public void AddRef()
-        {
-            refNum++;
-        }
-
-        public void SubRef()
-        {
-            refNum--;
-        }
-
-        public bool CanRelease()
-        {
-            return refNum <= 0;
-        }
-
-        public void Release()
-        {
-            refNum = 0;
-            obj = null;
-            Addressables.Release(handle);
-        }
-    }
-
 }
