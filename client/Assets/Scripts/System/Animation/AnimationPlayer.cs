a2f02956d896970254302200af6eed22c3c7bcf4
zhangwenbin <991355745@qq.com>
2021-09-14 17:40:10 +0800

    导入ilruntime

---
+++ client/Assets/Scripts/System/Animation/AnimationPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EG
+{
+    public class AnimationPlayer
+    {
+
+        private static readonly int MIXER_COUNT = 3;
+        public enum EFlag
+        {
+            None=0,
+            SamePlay=1<<0,
+            BlendTimeOverride=1<<1,
+        }
+       
+        public class PlayStatus
+        {
+            public enum State
+            {
+                
+            }
+        }
+    }
+}
